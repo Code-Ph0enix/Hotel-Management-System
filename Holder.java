c89a70736aa1cd2894312a7aa8ed9132ae80edbb
Eeshanya Joshi <122297998+Code-Ph0enix@users.noreply.github.com>
2024-05-16 13:39:52 +0530

    Holder

---
+++ Holder.java
@@ -0,0 +1,8 @@
+package ia2;
+public class Holder
+{
+    public Doubleroom luxury_doublerrom[]=new Doubleroom[10]; //Luxury
+    public Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; //Deluxe
+    public Singleroom luxury_singleerrom[]=new Singleroom[10]; //Luxury
+    public Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
+}
